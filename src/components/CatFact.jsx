2e39845770de3e620ce9be26bbab331eaf661998
Helez Baban <helez25sfj@gmail.com>
2025-08-26 13:40:05 +0200

    styling lagt til,

---
+++ src/components/CatFact.jsx
@@ -0,0 +1,5 @@
+const apiUrl = "https://catfact.ninja/facts?limit=5";
+
+export default function () {
+  fetch(apiUrl);
+}
--- src/components/CoockieClicker.jsx
+++ src/components/CoockieClicker.jsx
@@ -1,10 +1,15 @@
-// const [count, setCount] = useState(0);
+import { useState } from "react";
 
 export default function CoockieClicker() {
+  const [count, setCount] = useState(0);
+
+  //   setCount((prev) => prev + 1);
+
   return (
     <>
-      <button>
-        <img className="w-" src="./public/cupcake.jpg" alt="" />
+      <button onClick={() => setCount(count + 1)}>
+        <img className="w-sm rounded-full" src="./public/cupcake.jpg" alt="" />
+        <p className="text-4xl">Yum! You have {count} delicious cupcakes 😍</p>
       </button>
     </>
   );
