e80b4cf84c9f53a2771090a2c3a248f0a0799fd7
Helez Baban <helez25sfj@gmail.com>
2025-08-26 15:04:49 +0200

    implemented useeffect

--- src/components/CatFact.jsx
+++ src/components/CatFact.jsx
@@ -1,9 +1,21 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const apiUrl = "https://catfact.ninja/facts?limit=5";
 
 export default function CatFacts() {
   const [data, setData] = useState(null);
+
+  useEffect(() => {
+    async function fetchCatFact() {
+      const response = await fetch(apiUrl);
+
+      const jsonData = await response.json();
+      console.log(jsonData.data[0].fact);
+
+      setData(jsonData.data[0].fact);
+    }
+    fetchCatFact();
+  }, []);
 
   return (
     <>
