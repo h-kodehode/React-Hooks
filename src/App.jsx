00df296c0c978ac08f1f99a651dc81b4a3f9a078
Helez Baban <helez25sfj@gmail.com>
2025-08-26 13:13:29 +0200

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import CoockieClicker from "./components/CoockieClicker";
+
+function App() {
+  return (
+    <div>
+      <CoockieClicker />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/CoockieClicker.jsx
@@ -0,0 +1,11 @@
+// const [count, setCount] = useState(0);
+
+export default function CoockieClicker() {
+  return (
+    <>
+      <button>
+        <img className="w-" src="./public/cupcake.jpg" alt="" />
+      </button>
+    </>
+  );
+}
