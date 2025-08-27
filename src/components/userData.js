0b0a4a61c29b2e93d2cc49d817022aa8ff2c6668
Helez Baban <helez25sfj@gmail.com>
2025-08-27 21:08:24 +0200

    created mockData

---
+++ src/components/userData.js
@@ -0,0 +1,7 @@
+const userData = [
+  { username: "Ola Normann", email: "ola.normann@norge.no" },
+  { username: "Torleif", email: "torleif@kodehode.no" },
+  { username: "Jan Egil", email: "jan.egil@kodehode.no" },
+  { username: "Sander", email: "sander@kodehode.no" },
+];
+export default userData;
